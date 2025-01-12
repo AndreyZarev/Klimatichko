d6ac78aebc8639b67f46db6505aee555147a3ff7
tocode <andrey.zarev@outlook.com>
2025-01-12 18:39:18 +0200

    added a js file and a hidden class

---
+++ index-js/pullling-html.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let inventarAc = document.getElementsByClassName("inventor-ac")[0];
+    let categorySection = document.getElementById("category-section");
+
+    function clickOnInventarAc(e) {
+        e.preventDefault();
+        categorySection.classList.add("invisible");
+    }
+
+    inventarAc.addEventListener("click", clickOnInventarAc);
+});
