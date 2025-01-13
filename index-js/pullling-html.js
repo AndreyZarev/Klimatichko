96625acde05eda7cea4cb3c0e356465499e7363a
tocode <andrey.zarev@outlook.com>
2025-01-13 10:53:57 +0200

    added labels

--- index-js/pullling-html.js
+++ index-js/pullling-html.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let inventarAc = document.getElementsByClassName("inventor-ac")[0];
-    let categorySection = document.getElementById("category-section");
+// document.addEventListener("DOMContentLoaded", () => {
+//     let inventarAc = document.getElementsByClassName("inventor-ac")[0];
+//     let categorySection = document.getElementById("category-section");
 
-    function clickOnInventarAc(e) {
-        e.preventDefault();
-        categorySection.classList.add("invisible");
-    }
+//     function clickOnInventarAc(e) {
+//         e.preventDefault();
+//         categorySection.classList.add("invisible");
+//     }
 
-    inventarAc.addEventListener("click", clickOnInventarAc);
-});
+//     inventarAc.addEventListener("click", clickOnInventarAc);
+// });
