bb9aef854a7fbf01d7eda259d623012f0beadcd7
tocode <andrey.zarev@outlook.com>
2025-01-13 19:00:18 +0200

    added a new section

--- index-js/pullling-html.js
+++ index-js/pullling-html.js
@@ -1,11 +1,16 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     let inventarAc = document.getElementsByClassName("inventor-ac")[0];
-//     let categorySection = document.getElementById("category-section");
+document.addEventListener("DOMContentLoaded", () => {
+    let singleProduct = document.getElementsByClassName("ac-product")[0];
+    let productsCategory = document.getElementById("products-section");
+    let container = document.getElementsByClassName("container-wrap")[0]
+    let singleProductSection = document.getElementsByClassName("single-product")
+    function getToSingleProductPage(e, id) {
+        e.preventDefault();
+        debugger
+        let currectProduct = document.getElementById(id);
+        container.appendChild(currectProduct)
+        productsCategory.classList.add("invisible");
+        singleProductSection.classList.remove("invisible");
+    }
 
-//     function clickOnInventarAc(e) {
-//         e.preventDefault();
-//         categorySection.classList.add("invisible");
-//     }
-
-//     inventarAc.addEventListener("click", clickOnInventarAc);
-// });
+    singleProduct.addEventListener("click", getToSingleProductPage);
+});
