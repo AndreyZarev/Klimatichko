555e932c50f3b090a4d5cd0cffc1bc3ee6295887
tocode <andrey.zarev@outlook.com>
2025-01-14 22:15:42 +0200

    added CSS to call buttons, and hover effect

--- index-js/pullling-html.js
+++ index-js/pullling-html.js
@@ -1,16 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let singleProduct = document.getElementsByClassName("ac-product")[0];
-    let productsCategory = document.getElementById("products-section");
-    let container = document.getElementsByClassName("container-wrap")[0]
-    let singleProductSection = document.getElementsByClassName("single-product")
-    function getToSingleProductPage(e, id) {
-        e.preventDefault();
-        debugger
-        let currectProduct = document.getElementById(id);
-        container.appendChild(currectProduct)
-        productsCategory.classList.add("invisible");
-        singleProductSection.classList.remove("invisible");
-    }
+    // let singleProduct = document.getElementsByClassName("ac-product")[0];
+    // let productsCategory = document.getElementById("products-section");
+    // let container = document.getElementsByClassName("container-wrap")[0]
+    // let singleProductSection = document.getElementsByClassName("single-product")
+    // function getToSingleProductPage(e, id) {
+    //     e.preventDefault();
+    //     debugger
+    //     let currectProduct = document.getElementById(id);
+    //     container.appendChild(currectProduct)
+    //     productsCategory.classList.add("invisible");
+    //     singleProductSection.classList.remove("invisible");
+    // }
 
-    singleProduct.addEventListener("click", getToSingleProductPage);
-});
+
+
+    // singleProduct.addEventListener("click", getToSingleProductPage);
+
+
+    // call Section starts here 
+
+
+    let divOfImgButtons = document.getElementById("call-us-section-righ-div")
+    console.log(divOfImgButtons);
+
+    debugger
+    divOfImgButtons.addEventListener("mouseover", function () {
+        let arrFromDivOfButtons = divOfImgButtons.children
+
+
+        for (let index = 0; index < array.length; index++) {
+            const element = arrFromDivOfButtons[index].classList.add("invisible");
+            debugger
+
+        }
+
+    });
+
+}
+
+)
