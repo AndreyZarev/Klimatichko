a077ff21f16e7c1deabbf94d9e853b3214733f4b
tocode <andrey.zarev@outlook.com>
2025-01-16 22:39:53 +0200

    added dynamic html pulls

---
+++ index-js/dynamic-products.js
@@ -0,0 +1,26 @@
+
+
+
+window.addEventListener("load", function () {
+
+    let dynamicSection = document.getElementById("dynamic-section")
+
+    fetch("../inventor-ac/inventor-acs.html")
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to load HTML file.");
+            }
+            return response.text(); // Get the HTML content as text
+        })
+        .then(data => {
+            dynamicSection.innerHTML = data;
+        })
+        .catch(error => console.error(error));
+
+
+    let productList = document.getElementById("product-list")
+
+
+
+})
--- index-js/pullling-html.js
+++ index-js/pullling-html.js
@@ -1,41 +1,116 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     // let singleProduct = document.getElementsByClassName("ac-product")[0];
-//     // let productsCategory = document.getElementById("products-section");
-//     // let container = document.getElementsByClassName("container-wrap")[0]
-//     // let singleProductSection = document.getElementsByClassName("single-product")
-//     // function getToSingleProductPage(e, id) {
-//     //     e.preventDefault();
-//     //     debugger
-//     //     let currectProduct = document.getElementById(id);
-//     //     container.appendChild(currectProduct)
-//     //     productsCategory.classList.add("invisible");
-//     //     singleProductSection.classList.remove("invisible");
-//     // }
+document.addEventListener("DOMContentLoaded", () => {
+    // let singleProduct = document.getElementsByClassName("ac-product")[0];
+    // let productsCategory = document.getElementById("products-section");
+    // let container = document.getElementsByClassName("container-wrap")[0]
+    // let singleProductSection = document.getElementsByClassName("single-product")
+    // function getToSingleProductPage(e, id) {
+    //     e.preventDefault();
+    //     debugger
+    //     let currectProduct = document.getElementById(id);
+    //     container.appendChild(currectProduct)
+    //     productsCategory.classList.add("invisible");
+    //     singleProductSection.classList.remove("invisible");
+    // }
 
 
 
-//     // singleProduct.addEventListener("click", getToSingleProductPage);
+    // singleProduct.addEventListener("click", getToSingleProductPage);
 
 
-//     // call Section starts here
+    // call Section starts here
 
 
-//     let divOfImgButtons = document.getElementById("call-us-section-righ-div")
-//     console.log(divOfImgButtons);
+    //     let divOfImgButtons = document.getElementById("call-us-section-righ-div")
+    //     console.log(divOfImgButtons);
 
-//     debugger
-//     divOfImgButtons.addEventListener("mouseover", function () {
-//         let arrFromDivOfButtons = divOfImgButtons.children
+    //     debugger
+    //     divOfImgButtons.addEventListener("mouseover", function () {
+    //         let arrFromDivOfButtons = divOfImgButtons.children
 
 
-//         for (let index = 0; index < array.length; index++) {
-//             const element = arrFromDivOfButtons[index].classList.add("invisible");
-//             debugger
+    //         for (let index = 0; index < array.length; index++) {
+    //             const element = arrFromDivOfButtons[index].classList.add("invisible");
+    //             debugger
 
-//         }
+    //         }
 
-//     });
+    //     });
 
-// }
+    let typeAcOptions = document.getElementsByClassName("type-ac-options")[0];
+    let labelsAcOptions = document.getElementsByClassName("labels-ac-options")[0];
+    let searchButton = document.getElementsByClassName("search-button")[0];
 
-// )
+
+    // typeAcOptions.addEventListener("change", function () {
+    //     var selectedTypeValue = typeAcOptions.value;
+    //     console.log("Selected Type Value:", selectedTypeValue);
+    //     console.log(selectedTypeValue);
+
+    // });
+
+    // // Get selected value for the second dropdown
+    // labelsAcOptions.addEventListener("change", function () {
+    //     var selectedLabelValue = labelsAcOptions.value;
+    //     console.log("Selected Label Value:", selectedLabelValue);
+    //     console.log(selectedLabelValue);
+    // })
+
+    searchButton.addEventListener("click", search)
+    function search() {
+        let selectedTypeValue = typeAcOptions.value;
+        let selectedLabelValue = labelsAcOptions.value;
+        // console.log(selectedTypeValue, selectedLabelValue);
+
+        if (selectedTypeValue == "Всички климатици") {
+            console.log("all acs");
+
+        } else if (selectedTypeValue == "Инверторни климатици") {
+            console.log("inventar ac");
+
+
+        } else if (selectedTypeValue == "Конвектори") {
+            console.log("convectors");
+
+
+        } else if (selectedTypeValue == "Стенни климатици") {
+            console.log("wall acs");
+
+
+        } else if (selectedTypeValue == "Подови климатици") {
+            console.log("flor acs");
+
+
+        } else if (selectedTypeValue == "Колонни климатици") {
+            console.log("colon acs");
+
+
+        } else if (selectedTypeValue == "Касетъчни климатици") {
+            console.log("kaset acs");
+
+
+        } else if (selectedTypeValue == "Всички климатици") {
+            console.log("all acs");
+
+        }
+
+
+        // Подово-таванни климатици</
+
+        // За много ниски температури
+
+        if (selectedLabelValue == "Всички марки") {
+            console.log("all labels");
+
+        } else if (selectedLabelValue == "ARATSU") {
+
+        }
+
+        // console.log("nothing");
+
+
+    }
+
+
+}
+
+)
